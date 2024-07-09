3593ddb2ea24b1f6ca735e56c6e4f0258075b03d
kolostring <yoerodriguezjunior@gmail.com>
2024-07-09 08:33:20 -0400

    feat: navbar session reactive

--- components/index.js
+++ components/index.js
@@ -1,3 +1,28 @@
 import { loadNavBar } from "./navbar.js";
 
-loadNavBar(123);
+const sessionToken = sessionStorage.getItem("fooloc_token");
+
+if(sessionToken === undefined){
+  loadNavBar(undefined);
+}
+else{
+  const response = await fetch("http://127.0.0.1:8000/api/account/session", {
+    method: "GET",
+    headers: {
+      Authorization: "Token " + sessionToken,
+    },
+  })
+
+  const data = await response.json()
+  console.log(response);
+
+  if(response.ok){
+    console.log(data);
+    loadNavBar(data);
+  }
+  else{
+    loadNavBar(undefined);
+  }
+  
+}
+
--- components/navbar.js
+++ components/navbar.js
@@ -6,15 +6,16 @@ const anonymousSection = String.raw`
 const userMenu = (userName, email, profilePic) => String.raw`
   <div style="width: 5rem;" class="ml-auto drop-down">
     <div class="flex w-full">
-      <button style="height: 3.8rem" class="user ml-auto">
-        </button>
+      <button style="padding: 0;height: 3.8rem; aspect-ratio: 1/1; overflow: hidden; grid-row: 1/4" class="user ml-auto">
+        ${profilePic !== "" ? `<img style="width: 100%; height: 100%; object-fit: cover;" src=http://127.0.0.1:8000/${profilePic} />` : ""}
+      </button>
     </div>
 
     <div style="right: 100%; top: 0%;" class="drop-down-menu-wrapper rounded">
       <div style="width: 18rem">
         <div style="display: grid; grid-template-columns: auto 1fr; padding: 1rem; gap: 0.2rem 0.8rem">
-          <div style="height: 3rem; grid-row: 1/4" class="user">
-            ${profilePic !== undefined ? `<img src=${profilePic}/>` : ""}
+          <div style="height: 3rem; aspect-ratio: 1/1; overflow: hidden; grid-row: 1/4" class="user">
+            ${profilePic !== "" ? `<img style="width: 100%; height: 100%; object-fit: cover;" src=http://127.0.0.1:8000/${profilePic} />` : ""}
           </div>
           <p> <b>${userName}</b></p>
           <p>${email}</p>
@@ -29,7 +30,7 @@ const userMenu = (userName, email, profilePic) => String.raw`
   </div>
 `;
 
-export function loadNavBar(loginToken) {
+export function loadNavBar(user) {
   const slots = document.querySelectorAll(".slot__navbar");
 
   slots.forEach((slot) => {
@@ -38,8 +39,8 @@ export function loadNavBar(loginToken) {
       <button class="big-btn">Explore</button>
 
       ${
-        loginToken !== undefined
-          ? userMenu("john", "johndoe@gmail.com")
+        user !== undefined
+          ? userMenu(user.username, user.email, user.image_url)
           : anonymousSection
       }
      `;
